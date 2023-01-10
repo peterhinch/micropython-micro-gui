e2fb78818a2ca5fae9052124f29b34234bd6eda9
peterhinch <peter@hinch.me.uk>
2023-01-10 18:22:05 +0000

    ILI9486: Make pinouts consistent with PCB.

--- setup_examples/ili9486_pico.py
+++ setup_examples/ili9486_pico.py
@@ -11,10 +11,10 @@ import gc
 from drivers.ili94xx.ili9486 import ILI9486 as SSD
 freq(250_000_000)  # RP2 overclock
 
-pdc = Pin(17, Pin.OUT, value=0)
-pcs = Pin(14, Pin.OUT, value=1)
-prst = Pin(7, Pin.OUT, value=1)
-spi = SPI(0, sck=Pin(6), mosi=Pin(3), miso=Pin(4), baudrate=30_000_000)
+pdc = Pin(8, Pin.OUT, value=0)  # Arbitrary pins
+prst = Pin(9, Pin.OUT, value=1)
+pcs = Pin(10, Pin.OUT, value=1)
+spi = SPI(0, sck=Pin(6), mosi=Pin(7), miso=Pin(4), baudrate=30_000_000)
 gc.collect()  # Precaution before instantiating framebuf
 ssd = SSD(spi, pcs, pdc, prst)
 gc.collect()
@@ -25,5 +25,5 @@ nxt = Pin(19, Pin.IN, Pin.PULL_UP)  # Move to next control
 sel = Pin(16, Pin.IN, Pin.PULL_UP)  # Operate current control
 prev = Pin(18, Pin.IN, Pin.PULL_UP)  # Move to previous control
 increase = Pin(20, Pin.IN, Pin.PULL_UP)  # Increase control's value
-decrease = Pin(21, Pin.IN, Pin.PULL_UP)  # Decrease control's value
+decrease = Pin(17, Pin.IN, Pin.PULL_UP)  # Decrease control's value
 display = Display(ssd, nxt, sel, prev, increase, decrease)  # Pushbutton control
