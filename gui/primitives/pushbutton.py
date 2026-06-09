f7668293e2f1a2895a41c2c1e60807adfe108de1
Peter Hinch <peter@hinch.me.uk>
2026-06-09 18:33:23 +0100

    Update pushbutton.py.

--- gui/primitives/pushbutton.py
+++ gui/primitives/pushbutton.py
@@ -1,22 +1,25 @@
 # pushbutton.py
 
-# Copyright (c) 2018-2022 Peter Hinch
+# Copyright (c) 2018-2023 Peter Hinch
 # Released under the MIT License (MIT) - see LICENSE file
 
-import uasyncio as asyncio
-import utime as time
+import asyncio
+import time
 from . import launch, Delay_ms
+
 try:
     from machine import TouchPad
-except ImportError:
+except (ImportError, AttributeError):
     pass
+
 
 class Pushbutton:
     debounce_ms = 50
     long_press_ms = 1000
     double_click_ms = 400
+
     def __init__(self, pin, suppress=False, sense=None):
-        self.pin = pin # Initialise for input
+        self._pin = pin  # Initialise for input
         self._supp = suppress
         self._dblpend = False  # Doubleclick waiting for 2nd click
         self._dblran = False  # Doubleclick executed user function
@@ -25,10 +28,61 @@ class Pushbutton:
         self._df = False
         self._ld = False  # Delay_ms instance for long press
         self._dd = False  # Ditto for doubleclick
-        self.sense = pin.value() if sense is None else sense  # Convert from electrical to logical value
-        self.state = self.rawstate()  # Initial state
+        # Convert from electrical to logical value
+        self._sense = pin.value() if sense is None else sense
+        self._state = self.rawstate()  # Initial state
         self._run = asyncio.create_task(self._go())  # Thread runs forever
 
+    async def _go(self):
+        while True:
+            self._check(self.rawstate())
+            # Ignore state changes until switch has settled. Also avoid hogging CPU.
+            # See https://github.com/peterhinch/micropython-async/issues/69
+            await asyncio.sleep_ms(Pushbutton.debounce_ms)
+
+    def _check(self, state):
+        if state == self._state:
+            return
+        # State has changed: act on it now.
+        self._state = state
+        if state:  # Button pressed: launch pressed func
+            if self._tf:
+                launch(self._tf, self._ta)
+            # If there's a long func: start long press delay if no double click running
+            # (case where a short click is rapidly followed by a long one, iss 101).
+            if self._ld and not (self._df and self._dd()):
+                self._ld.trigger(Pushbutton.long_press_ms)
+            if self._df:
+                if self._dd():  # Second click: timer running
+                    self._dd.stop()
+                    self._dblpend = False
+                    self._dblran = True  # Prevent suppressed launch on release
+                    launch(self._df, self._da)
+                else:
+                    # First click: start doubleclick timer
+                    self._dd.trigger(Pushbutton.double_click_ms)
+                    self._dblpend = True  # Prevent suppressed launch on release
+        else:  # Button release. Is there a release func?
+            if self._ff:
+                if self._supp:
+                    d = self._ld
+                    # If long delay exists, is running and doubleclick status is OK
+                    if not self._dblpend and not self._dblran:
+                        if (d and d()) or not d:
+                            launch(self._ff, self._fa)
+                else:
+                    launch(self._ff, self._fa)
+            if self._ld:
+                self._ld.stop()  # Avoid interpreting a second click as a long push
+            self._dblran = False
+
+    def _ddto(self):  # Doubleclick timeout: no doubleclick occurred
+        self._dblpend = False
+        if self._ff and self._supp and not self._state:
+            if not self._ld or (self._ld and not self._ld()):
+                launch(self._ff, self._fa)
+
+    # ****** API ******
     def press_func(self, func=False, args=()):
         if func is None:
             self.press = asyncio.Event()
@@ -67,58 +121,11 @@ class Pushbutton:
 
     # Current non-debounced logical button state: True == pressed
     def rawstate(self):
-        return bool(self.pin.value() ^ self.sense)
+        return bool(self._pin() ^ self._sense)
 
     # Current debounced state of button (True == pressed)
     def __call__(self):
-        return self.state
-
-    def _ddto(self):  # Doubleclick timeout: no doubleclick occurred
-        self._dblpend = False
-        if self._supp and not self.state:
-            if not self._ld or (self._ld and not self._ld()):
-                launch(self._ff, self._fa)
-
-    def _check(self, state):
-        if state == self.state:
-            return
-        # State has changed: act on it now.
-        self.state = state
-        if state:  # Button pressed: launch pressed func
-            if self._tf:
-                launch(self._tf, self._ta)
-            if self._ld:  # There's a long func: start long press delay
-                self._ld.trigger(Pushbutton.long_press_ms)
-            if self._df:
-                if self._dd():  # Second click: timer running
-                    self._dd.stop()
-                    self._dblpend = False
-                    self._dblran = True  # Prevent suppressed launch on release
-                    launch(self._df, self._da)
-                else:
-                    # First click: start doubleclick timer
-                    self._dd.trigger(Pushbutton.double_click_ms)
-                    self._dblpend = True  # Prevent suppressed launch on release
-        else:  # Button release. Is there a release func?
-            if self._ff:
-                if self._supp:
-                    d = self._ld 
-                    # If long delay exists, is running and doubleclick status is OK
-                    if not self._dblpend and not self._dblran:
-                        if (d and d()) or not d:
-                            launch(self._ff, self._fa)
-                else:
-                    launch(self._ff, self._fa)
-            if self._ld:
-                self._ld.stop()  # Avoid interpreting a second click as a long push
-            self._dblran = False
-
-    async def _go(self):
-        while True:
-            self._check(self.rawstate())
-            # Ignore state changes until switch has settled. Also avoid hogging CPU.
-            # See https://github.com/peterhinch/micropython-async/issues/69
-            await asyncio.sleep_ms(Pushbutton.debounce_ms)
+        return self._state
 
     def deinit(self):
         self._run.cancel()
@@ -126,6 +133,7 @@ class Pushbutton:
 
 class ESP32Touch(Pushbutton):
     thresh = (80 << 8) // 100
+
     @classmethod
     def threshold(cls, val):
         if not (isinstance(val, int) and 0 < val < 100):
